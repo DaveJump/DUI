1b88bdf516c6dacfe342a5e033b8e7caceb76431
DaveJump <DaveJump>
2017-03-20 21:04:53 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Tabs } from './DUI';
+
+// Render the main component into the dom
+ReactDOM.render(<Tabs />, document.getElementById('app'));
